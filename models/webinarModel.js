f63751f16d6ab3c67a865e4d3e22c26523112305
anjalisrinivas9353-eng <anjalisrinivas9353@gmail.com>
2026-07-14 00:37:18 +0530

    Debug database query

--- models/webinarModel.js
+++ models/webinarModel.js
@@ -1,9 +1,5 @@
 const db = require("../config/db");
 
-console.log("Imported DB =", db);
-console.log("Type of db =", typeof db);
-console.log("Type of db.query =", typeof db.query);
-
 const createRegistration = async (data) => {
   const query = `
     INSERT INTO webinar_registrations
@@ -19,9 +15,13 @@ const createRegistration = async (data) => {
     data.place,
   ];
 
-  const result = await db.query(query, values);
-
-  return result.rows[0];
+  try {
+    const result = await db.query(query, values);
+    return result.rows[0];
+  } catch (err) {
+    console.error("DATABASE QUERY ERROR:", err);
+    throw err;
+  }
 };
 
 module.exports = {
